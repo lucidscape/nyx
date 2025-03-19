0fd0ec12a0107fdc46036776feb72b34745752db
Rod Furlan <rfurlan@gmail.com>
2025-03-19 13:22:42 -0700

    agent

---
+++ agent/Program.cs
@@ -0,0 +1,43 @@
+﻿#pragma warning disable SKEXP0070
+
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Ollama;
+
+var builder = Kernel.CreateBuilder();
+var endpoint = new Uri("http://100.102.129.64:11434");
+var modelId = "qwq";
+
+builder.Services.AddOllamaChatCompletion(modelId, endpoint);
+
+builder.Plugins
+    .AddFromType<TimePlugin>();
+
+var kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var settings = new OllamaPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+Console.Write("> ");
+
+string? input = null;
+while ((input = Console.ReadLine()) is not null)
+{
+    Console.WriteLine();
+
+    try
+    {
+        ChatMessageContent chatResult = await chatCompletionService.GetChatMessageContentAsync(input, settings, kernel);
+        Console.Write($"\n>>> Result: {chatResult}\n\n> ");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error: {ex.Message}\n\n> ");
+    }
+}
+
+public class TimePlugin
+{
+    [KernelFunction, Description("Get the current time")]
+    public DateTimeOffset Time() => DateTimeOffset.Now;
+}
